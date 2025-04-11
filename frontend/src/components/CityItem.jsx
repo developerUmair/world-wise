e9d2a84d264708176bb6b45748c7b83453221d4d
developerUmair <umairmak143@gmail.com>
2025-04-11 17:00:13 +0500

    remove useless code

--- frontend/src/components/CityItem.jsx
+++ frontend/src/components/CityItem.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
